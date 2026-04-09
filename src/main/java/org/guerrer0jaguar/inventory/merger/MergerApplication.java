9b75d0c1b8a4e020d2e09292a7f00dbf6139f8b2
Julio C. Ramírez <juliocesar.rgz@gmail.com>
2026-04-09 17:06:47 -0600

    feat: initial import

---
+++ src/main/java/org/guerrer0jaguar/inventory/merger/MergerApplication.java
@@ -0,0 +1,13 @@
+package org.guerrer0jaguar.inventory.merger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MergerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/guerrer0jaguar/inventory/merger/MergerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.guerrer0jaguar.inventory.merger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MergerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
