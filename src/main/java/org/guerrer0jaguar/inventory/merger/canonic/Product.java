d4f9da85e6b26f82a3e58f298b12f563f32db116
Julio C. Ramírez <juliocesar.rgz@gmail.com>
2026-04-10 16:54:38 -0600

    feat: adding JPA annotations

--- src/main/java/org/guerrer0jaguar/inventory/merger/canonic/Product.java
+++ src/main/java/org/guerrer0jaguar/inventory/merger/canonic/Product.java
@@ -1,11 +1,20 @@
 package org.guerrer0jaguar.inventory.merger.canonic;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
 @EqualsAndHashCode(callSuper=true)
+@Entity
 public class Product extends ProductBase {   
     private ProviderSource provider;
-    private Double rating;    
+    private Double rating;  
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long internalId;
 }
--- src/main/java/org/guerrer0jaguar/inventory/merger/canonic/Reviews.java
+++ src/main/java/org/guerrer0jaguar/inventory/merger/canonic/Reviews.java
@@ -14,43 +14,4 @@ public class Reviews {
 
     private String reviewerEmail;
 
-    public Integer getRating() {
-        return rating;
-    }
-
-    public void setRating(Integer rating) {
-        this.rating = rating;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getReviewerName() {
-        return reviewerName;
-    }
-
-    public void setReviewerName(String reviewerName) {
-        this.reviewerName = reviewerName;
-    }
-
-    public String getReviewerEmail() {
-        return reviewerEmail;
-    }
-
-    public void setReviewerEmail(String reviewerEmail) {
-        this.reviewerEmail = reviewerEmail;
-    }
 }
