a2372cfaac0b314c58271083929e2f695f804600
Julio C. Ramírez <juliocesar.rgz@gmail.com>
2026-04-10 20:09:29 -0600

    chore: setting global web context

--- src/main/java/org/guerrer0jaguar/inventory/merger/InventoryController.java
+++ src/main/java/org/guerrer0jaguar/inventory/merger/InventoryController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.guerrer0jaguar.inventory.merger.canonic.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class InventoryController {
     
     private final ProductService service;
